37dd79f8510e633e89db1d499cdaf3a62394f82e
Julio Jaramillo <juliocesar0293@gmail.com>
2026-07-03 12:16:20 -0600

    Create User Endpoint

---
+++ ecom-application/src/main/java/com/juliodev/ecom_application/model/User.java
@@ -0,0 +1,39 @@
+package com.juliodev.ecom_application.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "user_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private UserRole role = UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ ecom-application/src/main/java/com/juliodev/ecom_application/model/UserRole.java
@@ -0,0 +1,5 @@
+package com.juliodev.ecom_application.model;
+
+public enum UserRole {
+    CUSTOMER, ADMIN
+}
